25671be532405bf404ceb8628aadbaba6bb579b3
Dmitriy Kulyasov <vvvvvvmaz@gmail.com>
2022-03-20 20:41:49 +0300

    HW#6 + log + section+ singleton

--- src/ru/basejava/storage/AbstractStorage.java
+++ src/ru/basejava/storage/AbstractStorage.java
@@ -6,8 +6,11 @@ import ru.basejava.model.Resume;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<T> implements Storage {
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+
     protected abstract T getSearchKey(String uuid);
 
     protected abstract void doUpdate(Resume r, T searchKey);
@@ -23,26 +26,31 @@ public abstract class AbstractStorage<T> implements Storage {
     protected abstract List<Resume> getAll();
 
     public void update(Resume r) {
+        LOG.info("Update " + r);
         T searchKey = getExistedSearchKey(r.getUuid());
         doUpdate(r, searchKey);
     }
 
     public void save(Resume r) {
+        LOG.info("Save " + r);
         T searchKey = getNotExistedSearchKey(r.getUuid());
         doSave(r, searchKey);
     }
 
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         T searchKey = getExistedSearchKey(uuid);
         doDelete(searchKey);
     }
 
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         T searchKey = getExistedSearchKey(uuid);
         return doGet(searchKey);
     }
 
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> list = getAll();
         list.sort(RESUME_COMPARATOR);
         return list;
@@ -51,6 +59,7 @@ public abstract class AbstractStorage<T> implements Storage {
     private T getExistedSearchKey(String uuid) {
         T searchKey = getSearchKey(uuid);
         if (!isExist(searchKey)) {
+            LOG.warning("Resume " + uuid + " not exist");
             throw new NotExistStorageException(uuid);
         }
         return searchKey;
@@ -59,6 +68,7 @@ public abstract class AbstractStorage<T> implements Storage {
     private T getNotExistedSearchKey(String uuid) {
         T searchKey = getSearchKey(uuid);
         if (isExist(searchKey)) {
+            LOG.warning("Resume " + uuid + " already exist");
             throw new ExistStorageException(uuid);
         }
         return searchKey;
